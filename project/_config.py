2215ee6bb6db1c3a55d84ed8f8cb8a864959738b
Cushionyten0 <Cushionyten0@gmail.com>
2016-10-15 23:20:30 -0400

    Fixed Secret Key

--- project/_config.py
+++ project/_config.py
@@ -7,7 +7,7 @@ DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'my_precious'
+SECRET_KEY = '\x16T3\x89}\xa94\xca,`Z\x97\xef\xff\xf0GO\xdbE\xc0\xc2\xb0\x8fr'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)

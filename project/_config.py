ac4d1c7d5c98e03958215dc2412edee482518e23
Cushionyten0 <Cushionyten0@gmail.com>
2016-10-15 23:18:09 -0400

    rearranged project

--- project/_config.py
+++ project/_config.py
@@ -1,11 +1,13 @@
 import os
 
+# grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
-SECRET_KEY = '\x1e\xe3B\xfc\xb6\xe7H\x17\xfc\xe2U\xe8a)-\xc7\x04\xa3\xce_T&tC'
+SECRET_KEY = 'my_precious'
 
+# define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)

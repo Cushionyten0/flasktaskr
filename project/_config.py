7d7284bac7ac627138a45a21e308bca8442566c1
Cushionyten0 <Cushionyten0@gmail.com>
2016-10-17 00:33:22 -0400

    Working Site

--- project/_config.py
+++ project/_config.py
@@ -3,6 +3,7 @@ import os
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# configuration
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
